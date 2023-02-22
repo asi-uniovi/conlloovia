88746c4d4786ed5c2cb594fa48983e00a0ff4eb2
Joaquín Entrialgo <joaquin@uniovi.es>
2023-02-22 12:02:20 +0100

    Move to python 3.10
    This is required by cloudmodel

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
     "pint",
     "pulp",
     "rich",
-    "cloudmodel @ git+https://jentrialgo@github.com/jldiaz-uniovi/cloudmodel.git@main#egg=cloudmodel",
+    "cloudmodel @ git+https://jentrialgo@github.com/jldiaz-uniovi/cloudmodel.git#egg=cloudmodel",
 ]
 
 test_requirements = [
@@ -25,16 +25,14 @@ test_requirements = [
 setup(
     author="Joaquín Entrialgo",
     author_email="joaquin@uniovi.es",
-    python_requires=">=3.6",
+    python_requires=">=3.10",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
     ],
     description="Use linear programming to allocate containers to cloud infrastructure",
     entry_points={
@@ -52,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/jentrialgo/conlloovia",
-    version="0.1.0",
+    version="0.2.0",
     zip_safe=False,
 )
